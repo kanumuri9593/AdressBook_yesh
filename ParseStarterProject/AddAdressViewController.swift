4c4e3e51cd8e154d0bb56a13d8f1e2f1dd0c554b
Yeswanth varma Kanumuri <yeshu@Yeswanths-MBP.fios-router.home>
2016-01-21 16:29:26 -0500

    curren locatio updated

--- ParseStarterProject/AddAdressViewController.swift
+++ ParseStarterProject/AddAdressViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 import MapKit
+import Parse
+import CoreLocation
 
-class AddAdressViewController: UIViewController {
+class AddAdressViewController: UIViewController, CLLocationManagerDelegate ,MKMapViewDelegate, UITextFieldDelegate {
+    
+    
+    
+    var locationManager:CLLocationManager!
+    
+    var latitude :CLLocationDegrees = 0
+    var longitude :CLLocationDegrees = 0
+    
+    var currentLocation: String = ""
+    
+    
+    
+    
     
     
     @IBOutlet weak var map: MKMapView!
@@ -36,23 +51,106 @@ class AddAdressViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        
+        
+        
+        
     }
     
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        var location:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        self.latitude = location.latitude
+        self.longitude = location.longitude
+        
+        print("locations = \(location.latitude) \(location.longitude)")
+        
+        let center = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
+        
+        self.map.removeAnnotations(map.annotations)
+        
+        var pinLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.latitude, location.longitude)
+        var objectAnnotation = MKPointAnnotation()
+        objectAnnotation.coordinate = pinLocation
+        objectAnnotation.title = "Your location"
+        
+        //--- CLGeocode to get address of current location ---//
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil)
+            {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                return
+            }
+            
+            if placemarks!.count > 0
+            {
+                let pm = placemarks![0] as CLPlacemark
+                self.displayLocationInfo(pm)
+            }
+            else
+            {
+                print("Problem with the data received from geocoder")
+            }
+        })
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        self.map.addAnnotation(objectAnnotation)
+        
     }
-    */
-
+    
+    func displayLocationInfo(placemark: CLPlacemark?)
+    {
+        if let containsPlacemark = placemark
+        {
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality! : ""
+            let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode! : ""
+            let administrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea! : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country! : ""
+            
+            print(locality)
+            print(postalCode)
+            print(administrativeArea)
+            print(country)
+            
+            let currenGpsAdress : String = "Current Location : \(locality), \(administrativeArea), \(postalCode),\(country)."
+            
+            print(currenGpsAdress)
+            
+            self.currentAdress.text = currenGpsAdress
+            
+            
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!)
+    {
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
+    
+    
+    
 }
+    
+
+
+
+
+
+
